e0c6e15403f2ca5eeeced525307ce580df0c36c1
SHAHAAN08 <154593846+SHAHAAN08@users.noreply.github.com>
2025-08-28 17:03:34 +0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
         <ul className='flex justify-center items-center gap-4'>
             <li className='flex gap-3'>
                 <Link href="/"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py1 font-bold cursor-pointer'>Home</button></Link>
-                <Link href="/shorten"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py1 font-bold cursor-pointer'>Try Now</button></Link>
+                <Link href="/Shorten"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py1 font-bold cursor-pointer'>Try Now</button></Link>
                 <Link href="https://github.com/SHAHAAN08"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py1 font-bold cursor-pointer'>Github</button></Link>
             </li>
 
